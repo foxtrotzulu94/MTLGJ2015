65e9e9cc54fe50d1687a154d8feb4c476472dd83
MadReza <rezmaza@gmail.com>
2015-01-24 10:59:54 -0500

    Fixed Respawn. Moved Files to appropriate folders. Added Vs2013 useless files to ignore.

---
+++ Assets/Scripts/Characters/Respawn.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Respawn : MonoBehaviour {
+
+    public float lifeTime = 2f;
+    private GameObject player;
+
+	// Use this for initialization
+	void Start ()
+	{
+	    player = GameObject.FindGameObjectWithTag("Player");
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+	    lifeTime -= Time.deltaTime;
+	    if (lifeTime <= 0)
+	        player.renderer.enabled = true;
+	}
+}
