7cee2fd8f83da6e12cab81f0c48ac20eaf5f09ff
DarkGizmo <francois.vaillancourt@hotmail.ca>
2015-01-25 11:27:09 -0500

    * Some fixes

---
+++ Assets/Scripts/TimeAffected.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TimeAffected : MonoBehaviour
+{
+
+    public void Update()
+    {
+        Animator animator = GetComponent<Animator>();
+        if (animator != null)
+        {
+            animator.speed = TimeManager.GetTimeDilatation(TimeType.Gameplay);
+        }
+    }
+}
